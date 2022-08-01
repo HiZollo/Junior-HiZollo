a16c29e66472aa0285813a88a1ee859567897313
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2022-08-01 21:21:49 +0800

    feature: update playlist command

--- src/commands/Music/Playlist.ts
+++ src/commands/Music/Playlist.ts
@@ -1,6 +1,10 @@
+import { EmbedBuilder } from "discord.js";
 import { Command } from "../../classes/Command";
 import { Source } from "../../classes/Source";
-import { CommandType } from "../../utils/enums";
+import { Track } from "../../features/music/Model/Track";
+import pageSystem from "../../features/utils/pageSystem";
+import { CommandType, PageSystemMode } from "../../utils/enums";
+import { PageSystemPagesOptions } from "../../utils/interfaces";
 
 export default class MusicPlaylist extends Command<[]> {
   constructor() {
@@ -27,6 +31,43 @@ export default class MusicPlaylist extends Command<[]> {
     }
 
     await source.defer();
-    await source.update('以下是目前的播放清單\n' + queue.map(track => track.title).join('\n'));
+
+    const nowPlaying = source.client.music.getNowPlaying(source.guild.id) as Track;
+
+    const embed = new EmbedBuilder()
+      .setAuthor({ name: 'HiZollo 的音樂中心', iconURL: source.client.user?.displayAvatarURL() })
+      .setHiZolloColor();
+    
+    const pages: PageSystemPagesOptions[][] = [];
+
+    queue.forEach((track, i) => {
+      if (i % 10 === 0) pages.push([]);
+      pages[~~(i / 10)].push({ name: track.videoLink });
+    });
+
+    await pageSystem({
+      mode: PageSystemMode.Description, 
+      source: source, 
+      embed: embed, 
+      description: `以下是目前的播放清單\n\n\`>> \`${nowPlaying.videoLink}`, 
+      pages: pages, 
+      contents: {
+        exit: '播放清單已關閉', 
+        idle: '播放清單已因閒置過久而關閉'
+      }, 
+      extendFooter: `總時長 ${this.secondToString(source.client.music.getTotalLength(source.guild.id))}`
+    });
+  }
+
+  private secondToString(time: number) {
+    const bases = [60, 60, 24];
+    const basesName = ['秒', '分', '小時', '天'];
+    let index = 0, result = '';
+    while (time >= bases[index] && index < 3) {
+      result = `${time % bases[index]} ${basesName[index]} ${result}`
+      time = ~~(time / bases[index]);
+      index++;
+    }
+    return `${time} ${basesName[index]} ${result}`;
   }
 }
