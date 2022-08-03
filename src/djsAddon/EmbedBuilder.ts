c36b4f81fcee92e707f452e679cf6f48300e882a
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2022-08-04 00:17:43 +0800

    feature: embed random footer with different weights

--- src/djsAddon/EmbedBuilder.ts
+++ src/djsAddon/EmbedBuilder.ts
@@ -1,6 +1,6 @@
 import { EmbedBuilder, GuildMember, User } from "discord.js"
 import config from "../config";
-import randomElement from "../features/utils/randomElement";
+import randomInt from "../features/utils/randomInt";
 
 Object.defineProperties(EmbedBuilder.prototype, {
   applyHiZolloSettings: {
@@ -43,15 +43,31 @@ Object.defineProperties(EmbedBuilder.prototype, {
   }
 });
 
-function randomFooter(): string {
-  const prefix = [config.bot.prefix, '/'][Date.now() & 1];
-  return randomElement(footers).replaceAll('<>', prefix);
-};
+const randomFooter = getRandomFooterFunction();
 
-const footers = [
-  `不知道指令的用法？請使用 <>help 查看`, 
-  `想得知第一手更新資訊，你可以使用 <>ann`, 
-  `指令沒有回應？你可以使用 <>bug 向我們回報`, 
-  `有任何建議都歡迎使用 <>suggest 告訴我們！`, 
-  `你可以使用 <>links 查看 HiZollo 的相關連結`
-];
+function getRandomFooterFunction(): () => string {
+  const footers = new Map([
+    [`不知道指令的用法？請使用 <>help 查看`, 20], 
+    [`想得知第一手更新資訊，你可以使用 <>ann`, 20], 
+    [`指令沒有回應？你可以使用 <>bug 向我們回報`, 20], 
+    [`有任何建議都歡迎使用 <>suggest 告訴我們！`, 20], 
+    [`你可以使用 <>links 查看 HiZollo 的相關連結`, 20], 
+    [`覺得自己很沒用嗎？試試看 <>useless！`, 1]
+  ]);
+
+  const weightSum = [...footers.values()].reduce((acc, cur) => acc + cur, 0);
+
+  function getText(ran: number): string {
+    return [...footers.entries()].find(([_text, weight]) => {
+      ran -= weight;
+      return ran < 0;
+    })![0];
+  }
+
+  function randomFooter(): string {
+    const prefix = [config.bot.prefix, '/'][Date.now() & 1];
+    let random = randomInt(0, weightSum - 1);
+    return getText(random).replaceAll('<>', prefix);
+  }
+  return randomFooter;
+};
