9bcde90e1e24ed103fa56529f425c423608b9539
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2022-09-07 14:08:47 +0800

    fix: reduce the chance of invalid url by trying multiple times

--- src/classes/Music/Model/YoutubeUtil.ts
+++ src/classes/Music/Model/YoutubeUtil.ts
@@ -18,6 +18,8 @@
  * along with Junior HiZollo. If not, see <https://www.gnu.org/licenses/>.
  */
 
+import ytpl, { YouTubeStream } from "play-dl";
+
 /**
  * 與 Youtube 相關的功能性函式
  */
@@ -36,5 +38,18 @@ export class YoutubeUtil extends null {
    */
   static isPlaylistUrl(url: string): boolean {
     return /^((?:https?:)?\/\/)?(?:(?:www|m|music)\.)?((?:youtube\.com|youtu.be))\/(?:(playlist|watch))?(.*)?((\?|\&)list=)(PL|UU|LL|RD|OL)[a-zA-Z\d_-]{10,}(&.*)?$/.test(url);
+  }
+
+  /**
+   * 嘗試抓取指定連結的 Youtube 音樂串流，最多會嘗試 5 次
+   * @param url 指定連結
+   * @returns 音樂串流
+   */
+  static async getStream(url: string): Promise<YouTubeStream | void> {
+    let stream: YouTubeStream | void;
+    for (let i = 0; i < 5 && !stream; i++) {
+      stream = await ytpl.stream(url).catch(() => {}) as YouTubeStream | void;
+    }
+    return stream;
   }
 }
