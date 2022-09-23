8ce9a51e6f662d13e369adc2aad59cf0dce8fea3
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-09-23 13:48:30 +0800

    chore: change tab to spaces

--- src/register-webhook.ts
+++ src/register-webhook.ts
@@ -24,32 +24,32 @@ import { APIWebhook } from 'discord-api-types/v10'
 import 'dotenv/config';
 
 (async () => {
-	if (!process.env.TOKEN) {
-		console.error("Please configure your bot token in .env before registering webhook");
-		process.exit(1);
-	}
+  if (!process.env.TOKEN) {
+    console.error("Please configure your bot token in .env before registering webhook");
+    process.exit(1);
+  }
 
-	const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
-	const channelId = process.argv[2];
+  const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
+  const channelId = process.argv[2];
 
-	if (!channelId) {
-		console.error("Please provide the id of target channel");
-		process.exit(1);
-	}
+  if (!channelId) {
+    console.error("Please provide the id of target channel");
+    process.exit(1);
+  }
 
-	const name = process.argv[3] ?? "My Sweet Webhook";
+  const name = process.argv[3] ?? "My Sweet Webhook";
 
-	try {
-		const res = await rest.post(Routes.channelWebhooks(channelId), {
-			body: { name }
-		}) as APIWebhook;
+  try {
+    const res = await rest.post(Routes.channelWebhooks(channelId), {
+      body: { name }
+    }) as APIWebhook;
 
-		console.log(`Name: ${res.name}`);
-		console.log(`ID: ${res.id}`);
-		console.log(`Token: ${res.token}`);
-	} catch(err) {
-		console.error(err);
-		process.exit(1);
-	}
+    console.log(`Name: ${res.name}`);
+    console.log(`ID: ${res.id}`);
+    console.log(`Token: ${res.token}`);
+  } catch(err) {
+    console.error(err);
+    process.exit(1);
+  }
 
 })()
