2fbf5e5a51631af85724a4d3da35e646274ed4dc
Mantou1233 <rwkn100@gmail.com>
2022-11-24 14:56:59 +0800

    better code typed

--- src/autocomplete/diep_tank.ts
+++ src/autocomplete/diep_tank.ts
@@ -23,9 +23,7 @@ import tanks from "../features/json/diepTanks.json";
 import { AutocompleteData } from "../utils/types";
 
 export default function(_client: HZClient): AutocompleteData {
-  const result = [];
-  for (const tank in tanks)
-    result.push({ name: tank });
+  const result = Object.values(tanks).map((v) => ({ name: v.name }));
 
   return { '坦克': result };
 }
