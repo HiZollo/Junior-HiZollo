c6442c3a96c33fc37ec41843d6c5173f2881d350
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2023-07-18 21:16:06 +0800

    chore: remove none mfa restrict of buttonrole

--- src/buttons/buttonrole.ts
+++ src/buttons/buttonrole.ts
@@ -1,16 +1,16 @@
 /*
- * 
+ *
  * Copyright 2022 HiZollo Dev Team <https://github.com/hizollo>
- * 
+ *
  * This file is a part of Junior HiZollo.
- * 
- * Junior HiZollo is free software: you can redistribute it and/or 
+ *
+ * Junior HiZollo is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * Junior HiZollo is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *
+ * Junior HiZollo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
@@ -18,15 +18,12 @@
  * along with Junior HiZollo. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { ButtonInteraction, GuildMemberRoleManager, GuildMFALevel } from "discord.js";
+import { ButtonInteraction, GuildMemberRoleManager } from "discord.js";
 
 export default async function buttonrole(interaction: ButtonInteraction<"cached">): Promise<void> {
   const roleId = interaction.customId.slice('buttonrole_'.length);
   const role = interaction.guild?.roles.resolve(roleId);
-  if (interaction.guild.mfaLevel === GuildMFALevel.Elevated) {
-    await interaction.reply({ content: '這個伺服器開啟了 2FA 驗證，因此我無法管理身分組', ephemeral: true });
-    return;
-  }
+
   if (!role?.editable) {
     await interaction.reply({ content: `我的權限不足，因此無法管理 ${role} 身分組`, ephemeral: true });
     return;
