b0349ebdca9481b63a3782c916ef44f61bc3342b
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2023-07-26 16:15:09 +0800

    chore: let GuildMember#tag support new username

--- src/djsAddon/GuildMember.ts
+++ src/djsAddon/GuildMember.ts
@@ -1,16 +1,16 @@
 /*
- * 
+ *
  * Copyright 2022 HiZollo Dev Team <https://github.com/hizollo>
- * 
+ *
  * This file is a part of Junior HiZollo.
- * 
- * Junior HiZollo is free software: you can redistribute it and/or 
+ *
+ * Junior HiZollo is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * Junior HiZollo is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *
+ * Junior HiZollo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
@@ -23,7 +23,11 @@ import { GuildMember } from "discord.js"
 Object.defineProperties(GuildMember.prototype, {
   tag: {
     get: function(this: GuildMember) {
-      return `${this.displayName}#${this.user.discriminator}`;
+      return typeof this.displayName === 'string'
+        ? this.user.discriminator === '0' || this.user.discriminator === null
+          ? this.displayName
+          : `${this.displayName}#${this.user.discriminator}`
+        : null;
     }
   }
 });
