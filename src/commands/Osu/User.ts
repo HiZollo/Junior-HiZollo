fc44444539350f62bbc5ebdb8973fb97f8376777
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2023-07-27 14:21:00 +0800

    feature: add gamemode arg to osu commmands

--- src/commands/Osu/User.ts
+++ src/commands/Osu/User.ts
@@ -1,16 +1,16 @@
 /*
- * 
+ *
  * Copyright 2022 HiZollo Dev Team <https://github.com/hizollo>
- * 
+ *
  * This file is a part of Junior HiZollo.
- * 
- * Junior HiZollo is free software: you can redistribute it and/or 
+ *
+ * Junior HiZollo is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * Junior HiZollo is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *
+ * Junior HiZollo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
@@ -22,27 +22,41 @@ import { Command } from "../../classes/Command";
 import { Source } from "../../classes/Source";
 import { CommandType } from "../../typings/enums";
 import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
+import { GameMode } from "@hizollo/osu-api";
 
-export default class OsuUser extends Command<[string]> {
+export default class OsuUser extends Command<[string, number]> {
   constructor() {
     super({
-      type: CommandType.Information, 
-      parent: 'osu', 
-      name: 'user', 
-      description: '取得一名玩家的 osu! 資料', 
-      aliases: ['u'], 
+      type: CommandType.Information,
+      parent: 'osu',
+      name: 'user',
+      description: '取得一名玩家的 osu! 資料',
+      aliases: ['u'],
       options: [{
-        type: ApplicationCommandOptionType.String, 
+        type: ApplicationCommandOptionType.String,
         name: '玩家',
         description: '要查詢的玩家 ID 或名稱',
         required: true
+      }, {
+        type: ApplicationCommandOptionType.Number,
+        name: '遊戲模式',
+        description: '要查詢的遊戲模式',
+        required: false,
+        choices: [
+          { name: 'standard', value: 0 },
+          { name: 'taiko', value: 1 },
+          { name: 'catch', value: 2 },
+          { name: 'mania', value: 3 },
+        ]
       }]
     });
   }
 
-  public async execute(source: Source, [player]: [string]): Promise<void> {
+  public async execute(source: Source, [player, mode]: [string, number]): Promise<void> {
+    const gameMode = mode ? mode as GameMode : GameMode.Standard;
+
     const [user] = await source.client.osu.users
-      .getUser({ user: player })
+      .getUser({ user: player, gameMode })
       .catch(() => []);
 
     if (!user) {
@@ -53,8 +67,8 @@ export default class OsuUser extends Command<[string]> {
 
     await source.defer();
 
-    const { 
-      username, pp, rank, country, countryRank, level, accuracy, playcount, 
+    const {
+      username, pp, rank, country, countryRank, level, accuracy, playcount,
       scoreRankCount: { ssh, ss, sh, s, a }
     } = user;
     const helper = new EmbedBuilder()
@@ -75,7 +89,7 @@ export default class OsuUser extends Command<[string]> {
         { name: 'pp', value: `${pp || 0}`, inline: true },
         { name: '世界排名', value: `${rank || '無'}`, inline: true },
         { name: `地區（${country}）排名`, value: `${countryRank || '無'}`, inline: true },
-        { name: '準確率', value: `${~~(accuracy*1000)/1000}%` }
+        { name: '準確率', value: `${~~(accuracy * 1000) / 1000}%` }
       ]);
     await source.update({ embeds: [helper] });
   }
