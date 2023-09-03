0e7d5b86484c93f7251f61fa1d55560d7dcc6c10
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2023-09-04 00:54:26 +0800

    chore: finish calc command

--- src/commands/Calc.ts
+++ src/commands/Calc.ts
@@ -18,27 +18,19 @@
  * along with Junior HiZollo. If not, see <https://www.gnu.org/licenses/>.
  */
 
+import { CalcError, Calculator, ErrorCodes } from "@hizollo/calculator";
 import { ApplicationCommandOptionType } from "discord.js";
 import { Command } from "../classes/Command";
 import { Source } from "../classes/Source";
 import removeMd from "../features/utils/removeMd";
 import { ArgumentParseType, CommandType } from "../typings/enums";
 
-const decNumber = '(?:[1-9]*\\d*\\.\\d+)|(?:(?:(?:[1-9]\\d*)|0)\\.?)';
-const hexNumber = '0(?:X|x)[\dAaBbCcDdEeFf]+';
-const octNumber = '0o[0-7]+';
-const binNumber = '0b[01]+';
-const number = `(?:(?:${decNumber})|(?:${hexNumber})|(?:${octNumber})|(?:${binNumber}))`;
-const operation = '[+\\-*\\/^]';
-const term = `\\(? *${number}(?: *${operation} *${number})* *\\)?`;
-const expression = `^ *\\(? *[+\\-]? *${term}(?: *${operation} *${term})* *\\)? *$`;
-
 export default class Calc extends Command<[string]> {
   constructor() {
     super({
       type: CommandType.Utility, 
       name: 'calc', 
-      description: '讓我來幫你計算一串算式，支援四則運算及雙層括號', 
+      description: '讓我來幫你計算一串算式，支援四則運算及各類數學函式', 
       aliases: ['c'], 
       options: [{ 
         type: ApplicationCommandOptionType.String, 
@@ -52,34 +44,46 @@ export default class Calc extends Command<[string]> {
     });
   }
 
-  public async execute(source: Source, [formula]: [string]): Promise<void> {
-    const validExpression = new RegExp(expression);
-    let finalContent;
+  private calculator = new Calculator()
 
+  public async execute(source: Source, [formula]: [string]): Promise<void> {
     try {
-      if (!validExpression.test(formula)) {
+      const result = this.calculator.calculate(formula).toString();
+      if (result.length > 2000) {
         await source.defer({ ephemeral: true });
-        await source.update(`這個算式似乎夾雜了一些其他的東西，如果你認為你的輸入皆無誤，請協助用 \`bug\` 指令回報給開發團隊`);
+        await source.update(`計算結果超出 2000 字元上限，因此我無法顯示`);
         return;
       }
-      else {
-        const original = removeMd(formula);
-        const answer = removeMd(eval(formula.replace(/\^/g, '**'))?.toString());
-        finalContent = `算式：\n> ${original}\n\n結果：\n>>> ${answer}`;
-        if (finalContent.length > 2000) {
-          await source.defer({ ephemeral: true });
-          await source.update(`計算結果超出 2000 字元上限，因此我無法顯示`);
-          return;
-        }
-      }
-    } catch (error) {
-      console.error(error);
+      await source.defer();
+      await source.update(`算式：\n> ${removeMd(formula)}\n\n結果：\n> ${removeMd(result)}`);
+    } catch (error: unknown) {
+      const err = error as CalcError<ErrorCodes>
       await source.defer({ ephemeral: true });
-      await source.update(`好像出了一點錯誤？我不知道發生了什麼，如果你認為這不是你輸入的問題，請協助用 \`bug\` 指令回報給開發團隊`);
-      return;
+      let string = `\`\`\`\n${formula}\n`
+      string += ' '.repeat(err.position < 0 ? formula.length : err.position)
+      string += `^ ${this.messages[err.code]}\n\`\`\``
+      await source.update(string);
     }
 
-    await source.defer();
-    await source.update(finalContent);
   }
+
+  private messages = {
+    // Lexer
+    [ErrorCodes.InvalidCharacter]: `未知的字元`, 
+    [ErrorCodes.InvalidOperator]: `未知的運算子`, 
+    [ErrorCodes.InvalidNumber]: `不是正確的數字`, 
+    [ErrorCodes.PositionNotationError]: '不是這個進位制該有的數字', 
+
+    // Parser
+    [ErrorCodes.ExtraTrailingTokens]: `這個運算式的尾端多了不該有的東西`, 
+    [ErrorCodes.InvalidToken]: `未知的常數或函數名稱`, 
+    [ErrorCodes.MissingOpenParenthesis]: `缺少左括號`, 
+    [ErrorCodes.MissingCloseParenthesis]: `缺少右括號`, 
+    [ErrorCodes.NotANumber]: `應該要是數字，但它不是`, 
+    [ErrorCodes.MissingExpressions]: `這個運算式的尾端缺少了一些東西`, 
+
+    // Implementation
+    [ErrorCodes.EmptyStack]: `指令好像出了一點問題，請你將你的算式和以下錯誤訊息使用 \`bug\` 指令回報給開發者：\n\`\`\`\nError: The expression stack is empty.\n\`\`\``, 
+    [ErrorCodes.NonEmptyStack]: `指令好像出了一點問題，請你將你的算式和以下錯誤訊息使用 \`bug\` 指令回報給開發者：\n\`\`\`Error: Non-empty stack after parsing.\n\`\`\``, 
+  } as const;
 }
